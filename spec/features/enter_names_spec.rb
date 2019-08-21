52deafffd4f6c4f8729f270d193bf88d1e8e9be6
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-08-21 23:04:02 +0100

    add helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,18 +1,12 @@
 feature 'homepage' do
  scenario 'see user names on-screen' do
-   visit '/'
-   fill_in('player_1', :with => 'Chris')
-   fill_in('player_2', :with => 'Paris')
-   click_button('submit')
+   sign_in_and_play
    expect(page).to have_content 'Chris'
    expect(page).to have_content 'Paris'
  end
 
  scenario 'want to see hit points of player 2' do
-  visit '/'
-  fill_in('player_1', :with => 'Chris')
-  fill_in('player_2', :with => 'Paris')
-  click_button('submit')
+  sign_in_and_play
   expect(page).to have_content 'HP'
  end
 end
