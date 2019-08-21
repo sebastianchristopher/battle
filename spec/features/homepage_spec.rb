e50db836a4ad4879d54af25b7ff70a3b2524ec0a
ParisaTork <parisa_tork@yahoo.co.uk>
2019-08-21 17:00:05 +0100

    viewing hit points tbc

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,4 +7,12 @@ feature 'homepage' do
    expect(page).to have_content 'Chris'
    expect(page).to have_content 'Paris'
  end
+
+ scenario 'want to see hit points of player 2' do
+  visit '/'
+  fill_in('player_1', :with => 'Chris')
+  fill_in('player_2', :with => 'Paris')
+  click_button('submit')
+  expect(page).to have_content 'HP'
+ end
 end
