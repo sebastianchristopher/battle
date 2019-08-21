8f8d6bb8ad587fb08314b88a0fee8b883bf56fa3
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-08-21 16:17:13 +0100

    wip"

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:play)
   end
 
   run! if app_file == $0
